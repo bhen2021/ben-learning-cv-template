c9f455885faf26edf56a20d8de13eec3ef7eba28
Bensor Datumanong <ben@youhq.co>
2021-02-12 10:44:14 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -4,11 +4,15 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
+body,h1,h2,h3,h4,h5,h6,p,i {
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+}
+
 a {
     text-decoration: none;
 }
 body {
-    background-color: orange;
+    background-color: darkgray;
     }
 .bottom-left {
     position: absolute;
@@ -16,12 +20,18 @@ body {
     left: 25px;
     top: 250px;
     }
-h2 {
-    color: whitesmoke;
-}
 div.a {
     text-align: left;
     padding-left: 25px;
+    margin: 20px;
+}
+hr.a {
+    border-top: 1px solid lightgray;
+    width: 92%;
+    text-align: center;
+}
+hr.b {
+    border: 5px solid grey;
 }
 </style>
 <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
@@ -34,7 +44,7 @@ div.a {
         grid-template-columns: 428px 877px;
         grid-template-rows: 1000px;
         grid-gap: 10px;
-        background-color: orange;
+        background-color: grey;
         padding: 10px;
     }
     .grid-container > div {
@@ -44,32 +54,54 @@ div.a {
         background-color: whitesmoke;
     }
     </style>
-
+    <!-- grid container -->
     <div class="grid-container">
         <!-- left column -->
         <div>
             <img src="bhen1.jpg" width="428" height="290" alt="master" >
-            <div class="bottom-left"><h2>Bensor O. Datumanong</h2></div>
+            <div class="bottom-left" style="color:white";><h2>Bensor O. Datumanong</h2></div>
         </div>
         <!-- end left column -->
      
 
+
+
+
         <!-- right column -->
         <div></br>
             <div class="a">
-            <h1><i class='fa fa-briefcase' style="font-size:36px"> Work Experience</i></h1>
+            <h1><i class='fa fa-briefcase' style="font-size:30px;color:black;"></i> Work Experience</h1>
             </div>
             <div class="a">
                 <b>PHP Developer / Letscobo.com</b></br>
                 <i class='far fa-calendar-alt'> Jan 2021 - Current</i></br>
                 <?= $cvtemp['work1']; ?>
             </div>
+            <hr class="a">
+            <div class="a">
+                <b>Laboratory Custodian / Asian Institute of Cumputer Studies</b></br>
+                <i class='far fa-calendar-alt'> Aug 2019 - June 2020</i></br>
+                <?= $cvtemp['work2']; ?>
+            </div>
+            <hr class="a">
+            <div class="a">
+                <b>MIS Assistant / Asia Source College of Arts & Technology</b></br>
+                <i class='far fa-calendar-alt'> Aug 2018 - June 2019</i></br>
+                <?= $cvtemp['work3']; ?>
+            </div>
+            <hr class="a">
+            <div class="a">
+                <b>Student Assistant / Asian Institute of Cumputer Studies</b></br>
+                <i class='far fa-calendar-alt'> Jun 2014 - June 2015</i></br>
+                <?= $cvtemp['work4']; ?>
+            </div></br>
+            <hr class="b">
             
 
 
 
         </div>
-        <!-- right column -->    
+        <!--end right column -->    
         
         
         
@@ -78,7 +110,7 @@ div.a {
     
 
     </div>
-
+    <!-- grid container -->
 
 </div>
 
