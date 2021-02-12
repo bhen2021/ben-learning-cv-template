699d14da5a72afbd50c64db108905d06a673ef39
Bensor Datumanong <ben@youhq.co>
2021-02-12 16:13:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 $cvtemp = [
 
-'work1' => 'Studying PHP language.',
-'work2' => 'I am responsible for monitoring and maintaining the computer systems, networks and laboratories.',
-'work3' => 'I am responsible for helping the MIS manager to plan, coordinate and direct all computer related activities.',
-'work4' => 'I am responsible for maintaining and monitoring computer laboratories, marketing, data incoding, paper works. '
+    'work1' => 'Studying PHP language.',
+    'work2' => 'I am responsible for monitoring and maintaining the computer systems, networks and laboratories.',
+    'work3' => 'Im helped the MIS manager to plan, coordinate and direct all computer related activities.',
+    'work4' => 'Im maintaining and monitoring computer laboratories, marketing, data incoding, paper works. '
+];
+
+
+$cvtemp1 = [
+    'educa1' => 'BSCS Bachelor Degree.',
+    'educa2' => 'Graduated.',
+    'educa3' => 'Graduated.',
+
+
 ];
 
 require 'index2.php';
